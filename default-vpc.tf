ff8359d911a2c85beaa23f410f2e63798201e3c1
Pierre Gentile <pierre.gentile.perso@gmail.com>
2018-03-02 23:56:03 +0100

    Archi multi instances

--- default-vpc.tf
+++ default-vpc.tf
@@ -3,7 +3,7 @@ resource "aws_default_vpc" "default" {
   enable_dns_hostnames = true
 
   tags {
-    Name        = "Terraform"
+    Name        = "DEFAULT"
     Provisioner = "terraform"
   }
 }
@@ -19,16 +19,24 @@ resource "aws_default_security_group" "default" {
     cidr_blocks = ["${var.my_ip}/32"]
   }
 
+  ingress {
+    description = "HTTP"
+    protocol    = "tcp"
+    from_port   = 80
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     description = "ALL"
-    protocol    = "-1"
+    protocol    = -1
     from_port   = 0
     to_port     = 0
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   tags {
-    Name        = "Terraform"
+    Name        = "DEFAULT"
     Provisioner = "terraform"
   }
 }
@@ -42,19 +50,23 @@ resource "aws_default_route_table" "route_table" {
   }
 
   tags {
-    Name        = "Terraform"
+    Name        = "DEFAULT"
     Provisioner = "terraform"
   }
 }
 
 data "aws_availability_zones" "availability_zones" {}
 
+locals {
+  availability_zones = "${sort(data.aws_availability_zones.availability_zones.names)}"
+}
+
 resource "aws_default_subnet" "default" {
-  count             = "${length(data.aws_availability_zones.availability_zones.names)}"
-  availability_zone = "${element(data.aws_availability_zones.availability_zones.names , count.index)}"
+  count             = "${length(local.availability_zones)}"
+  availability_zone = "${element(local.availability_zones, count.index)}"
 
   tags {
-    Name        = "Terraform / ${element(data.aws_availability_zones.availability_zones.names , count.index)}"
+    Name        = "DEFAULT / ${element(local.availability_zones, count.index)}"
     Provisioner = "terraform"
   }
 }
@@ -63,8 +75,35 @@ resource "aws_default_network_acl" "default" {
   default_network_acl_id = "${aws_default_vpc.default.default_network_acl_id}"
 
   ingress {
+    protocol   = "tcp"
+    rule_no    = 1000
+    action     = "allow"
+    cidr_block = "${var.my_ip}/32"
+    from_port  = 22
+    to_port    = 22
+  }
+
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 1001
+    action     = "deny"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 22
+    to_port    = 22
+  }
+
+  ingress {
+    protocol   = "tcp"
+    rule_no    = 2000
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 80
+    to_port    = 80
+  }
+
+  ingress {
     protocol   = -1
-    rule_no    = 100
+    rule_no    = 9999
     action     = "allow"
     cidr_block = "0.0.0.0/0"
     from_port  = 0
@@ -72,8 +111,26 @@ resource "aws_default_network_acl" "default" {
   }
 
   egress {
+    protocol   = "tcp"
+    rule_no    = 1000
+    action     = "allow"
+    cidr_block = "${var.my_ip}/32"
+    from_port  = 22
+    to_port    = 22
+  }
+
+  egress {
+    protocol   = "tcp"
+    rule_no    = 1001
+    action     = "deny"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 22
+    to_port    = 22
+  }
+
+  egress {
     protocol   = -1
-    rule_no    = 100
+    rule_no    = 9999
     action     = "allow"
     cidr_block = "0.0.0.0/0"
     from_port  = 0
@@ -81,7 +138,7 @@ resource "aws_default_network_acl" "default" {
   }
 
   tags {
-    Name        = "Terraform"
+    Name        = "DEFAULT"
     Provisioner = "terraform"
   }
 }
@@ -90,24 +147,73 @@ resource "aws_internet_gateway" "default" {
   vpc_id = "${aws_default_vpc.default.id}"
 
   tags {
-    Name        = "Terraform"
+    Name        = "DEFAULT"
     Provisioner = "terraform"
   }
 }
 
 resource "aws_default_vpc_dhcp_options" "default" {
   tags {
-    Name        = "Terraform"
+    Name        = "DEFAULT"
     Provisioner = "terraform"
   }
 }
 
-resource "aws_route53_zone" "vpc_zone" {
-  name   = "example.com"
-  vpc_id = "${aws_default_vpc.default.id}"
+resource "aws_flow_log" "default_vpc" {
+  log_group_name = "${aws_cloudwatch_log_group.default_vpc.name}"
+  iam_role_arn   = "${aws_iam_role.default_vpc_flow_log.arn}"
+  vpc_id         = "${aws_default_vpc.default.id}"
+  traffic_type   = "ALL"
+}
+
+resource "aws_cloudwatch_log_group" "default_vpc" {
+  name = "DefaultVPC"
 
   tags {
-    Name        = "Terraform"
     Provisioner = "terraform"
   }
+}
+
+resource "aws_iam_role" "default_vpc_flow_log" {
+  name = "DefaultVPCFlowLogRole"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "vpc-flow-logs.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "default_vpc_flow_log" {
+  name = "DefaultVPCFlowLogPolicy"
+  role = "${aws_iam_role.default_vpc_flow_log.id}"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents",
+        "logs:DescribeLogGroups",
+        "logs:DescribeLogStreams"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
 }
--- instance.tf
+++ instance.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "example" {
-  count         = 1
+  count         = 2
   ami           = "${data.aws_ami.amazon_linux.id}"
   instance_type = "t2.micro"
 
@@ -12,20 +12,14 @@ resource "aws_instance" "example" {
     Provisioner = "terraform"
   }
 
+  volume_tags {
+    Name        = "Terraform"
+    Provisioner = "terraform"
+  }
+
   root_block_device {
     volume_size = 8
   }
-}
-
-resource "aws_route53_record" "example" {
-  zone_id = "${aws_route53_zone.vpc_zone.zone_id}"
-  name    = "dev.${aws_route53_zone.vpc_zone.name}"
-  type    = "A"
-  ttl     = "30"
-
-  records = [
-    "${aws_instance.example.private_ip}",
-  ]
 }
 
 data "aws_ami" "amazon_linux" {
@@ -34,9 +28,9 @@ data "aws_ami" "amazon_linux" {
   filter {
     name = "name"
 
-    # values = ["amzn-ami-*-x86_64-gp2"]
+    // ECS AMI : amzn-ami-2017.09.*-amazon-ecs-optimized
     values = [
-      "amzn-ami-2017.09.*-amazon-ecs-optimized",
+      "amzn-ami-*-x86_64-gp2",
     ]
   }
 
@@ -54,4 +48,43 @@ data "aws_ami" "amazon_linux" {
 resource "aws_key_pair" "ssh_key" {
   key_name   = "ssh_key"
   public_key = "${file("~/.ssh/id_rsa.pub")}"
+}
+
+resource "aws_lb" "example" {
+  name            = "TerraformLB"
+  subnets         = ["${aws_default_subnet.default.*.id}"]
+  security_groups = ["${aws_default_security_group.default.id}"]
+
+  tags {
+    Provisioner = "terraform"
+  }
+}
+
+resource "aws_lb_listener" "example_http" {
+  load_balancer_arn = "${aws_lb.example.arn}"
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    target_group_arn = "${aws_lb_target_group.backend.arn}"
+    type             = "forward"
+  }
+}
+
+resource "aws_lb_target_group" "backend" {
+  name     = "TerraformLBBackend"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = "${aws_default_vpc.default.id}"
+
+  tags {
+    Provisioner = "terraform"
+  }
+}
+
+resource "aws_lb_target_group_attachment" "example" {
+  count            = "${aws_instance.example.count}"
+  target_group_arn = "${aws_lb_target_group.backend.arn}"
+  target_id        = "${element(aws_instance.example.*.id, count.index)}"
+  port             = 80
 }
