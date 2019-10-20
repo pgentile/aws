5a813e9dec7d9db75618dd49f6e1ea5ee61864f3
Pierre Gentile <pierre.gentile.perso@gmail.com>
2019-10-20 09:37:13 +0200

    Upgraded Core Linux Config plugin

--- install-terraform-plugins.sh
+++ install-terraform-plugins.sh
@@ -3,13 +3,13 @@
 set -e 
 
 plugins_dir="$HOME/.terraform.d/plugins/darwin_amd64"
-version="v0.3.0"
+version="v0.4.0"
 arch=darwin-amd64
 
 mkdir -p $plugins_dir
 
 cd $plugins_dir
-curl -L -O "https://github.com/coreos/terraform-provider-ct/releases/download/$version/terraform-provider-ct-$version-$arch.tar.gz"
+curl -L -O "https://github.com/poseidon/terraform-provider-ct/releases/download/$version/terraform-provider-ct-$version-$arch.tar.gz"
 
 tar xzvf terraform-provider-ct-$version-$arch.tar.gz
 rm terraform-provider-ct-$version-$arch.tar.gz
