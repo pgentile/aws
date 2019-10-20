40e803828e78568bb0493ce46b4ebe6923652403
Pierre Gentile <pierre.gentile.perso@gmail.com>
2019-10-20 13:08:37 +0200

    New arguments

--- ssh-config.tf
+++ ssh-config.tf
@@ -17,8 +17,8 @@ locals {
 }
 
 resource "local_file" "ssh_private_key" {
-  content  = tls_private_key.ssh.private_key_pem
-  filename = "${path.module}/output/ssh/private-key"
+  sensitive_content = tls_private_key.ssh.private_key_pem
+  filename          = "${path.module}/output/ssh/private-key"
 
   file_permission      = "0600"
   directory_permission = "0755"
